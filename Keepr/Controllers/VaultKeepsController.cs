81e7069225f5226c33fe972183f892a9533b9c16
alexmquan <alexmquan93@gmail.com>
2023-05-05 12:39:22 -0600

    Built backend skeleton

---
+++ Keepr/Controllers/VaultKeepsController.cs
@@ -0,0 +1,16 @@
+namespace Keepr.Controllers;
+// NOTE May need to adjust this authorize
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class VaultKeepsController
+{
+  private readonly VaultKeepsService _vaultKeepsService;
+  private readonly Auth0Provider _auth;
+
+  public VaultKeepsController(VaultKeepsService vaultKeepsService, Auth0Provider auth)
+  {
+    _vaultKeepsService = vaultKeepsService;
+    _auth = auth;
+  }
+}
